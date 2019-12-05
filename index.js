f762a2dcb102193b4ac8960a9121161bb5dc83aa
Qi <xuq2@oregonstate.edu>
2019-12-04 18:32:12 -0800

    update

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 document.getElementById('home').addEventListener('click', function(){
   window.location.href="index.html";
 });
+
+document.getElementById('drop-relics').addEventListener('click', function(){
+  window.location.href="contents.html";
+});
