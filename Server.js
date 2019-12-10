f5cbb01836554651ec82065b346d9b4092c1dc7f
Qi <xuq2@oregonstate.edu>
2019-12-10 15:03:10 -0800

    update

--- Server.js
+++ Server.js
@@ -4,12 +4,18 @@ var exphbs = require('express-handlebars');
 var app = express();
 var port = process.env.PORT || 3000;
 
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 
 
+app.get('/',function(req,res,next){
+  req.status(200).render('');
+});
 
 
-
-
+app.get('/',function(req,res,next){
+  req.status(200).render('');
+});
 
 
 
